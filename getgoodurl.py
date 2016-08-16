42ce946616b914f87a29fe4e93592c660fcec9e7
Ernesto Araiza <yo@ernestoaraiza.com>
2016-08-15 23:33:33 -0500

    adding utf8 support

--- getgoodurl.py
+++ getgoodurl.py
@@ -11,32 +11,59 @@ import csv
 import argparse
 import urllib2
 from bs4 import BeautifulSoup
+import errno
+from time import sleep
 
 parser = argparse.ArgumentParser()
 parser.add_argument("csvfile", help="El archivo csv")
+parser.add_argument("csvout", help="El archivo de salida")
 args = parser.parse_args()
 
 class GRReader:
 
 		def __init__(self, csvfile):
-			self.linereader = csv.DictReader(open(csvfile, 'rU'))
-			csvfile_write  = open('output.csv', 'wb')
+			self.linereader = self.UnicodeDictReader(open(csvfile))
+			csvfile_write  = open(args.csvout, 'wb')
 			self.linewriter = csv.writer(csvfile_write, delimiter=',')
 			self.linewriter.writerow(['id', 'title', 'author', 'avg_rating', 'ratings', 'url', 'img', 'pages', 'year'])
+
+		def UnicodeDictReader(self, utf8_data, **kwargs):
+			csv_reader = csv.DictReader(utf8_data, **kwargs)
+			for row in csv_reader:
+			    yield {key: unicode(value, 'utf-8') for key, value in row.iteritems()}
 
 		def lee(self):
 			for row in self.linereader:
 				print row
+
+		def __visita_URL(self, url):
+			intentos = 0
+			excepcion = True
+			sopa = ""
+
+			while(excepcion):
+				try:
+					sopa = BeautifulSoup(urllib2.urlopen(url).read())
+					excepcion = False
+				except SocketError as e:
+					if e.errno == errno.ECONNRESET:
+						intentos = intentos + 1
+						print "Intento " + intentos + ", sleeping..."
+						sleep(2000)
+					excepcion = True
+
+			return sopa
 
 		def busca(self):
 			for row in self.linereader:
 				base = 'https://www.goodreads.com/search?utf8=✓&q='
 				title = row['title'].strip()
 				title = title.replace(' ', '+')
+				title = title.encode('utf-8')
 				end = '&search_type=books&search%5Bfield%5D=title'
 				url = base+title+end
 
-				soup = BeautifulSoup(urllib2.urlopen(url).read())
+				soup = self.__visita_URL(url)
 				href = ""
 				good_title = ""
 				good_author = ""
@@ -53,16 +80,17 @@ class GRReader:
 					minirating_list = minirating.split(u' — ')
 					avg_rating = minirating_list[0][0:-11]
 					ratings = minirating_list[1][0:-8]
+					print row['author'].strip() +' vs ' + good_author
 					if (row['author'].strip() in good_author):
 						break
 
 				url2 = 'https://www.goodreads.com' + href
-				#print 'Visiting... ' + url2
-				soup2 = BeautifulSoup(urllib2.urlopen(url2).read())
+				print 'Visiting... ' + url2
+				soup2 = self.__visita_URL(url2)
 				img = soup2.find("img", {"id": "coverImage"})['src']
 				pages = soup2.find("span", {"itemprop": "numberOfPages"}).get_text()[0:-6]
 
-				self.linewriter.writerow([row['id'], good_title, good_author, avg_rating, ratings, href, img, pages, row['year']])
+				self.linewriter.writerow([row['id'], good_title.encode('utf-8'), good_author.encode('utf-8'), avg_rating, ratings, href, img, pages, row['year']])
 				print row['id'] + ' ' + good_title + ' by ' + good_author + ' ' + avg_rating + ' ' + ratings + ' ' +href + ' '+ img + ' ' + pages
 
 gr = GRReader(args.csvfile)
